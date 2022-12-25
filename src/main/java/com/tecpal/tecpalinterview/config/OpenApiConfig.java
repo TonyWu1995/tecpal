bdd786f6cc16fdad8031cf17fd6b24b643052e6a
Tony <gm10285@gmail.com>
2022-12-25 19:32:40 +0800

    fixed

---
+++ src/main/java/com/tecpal/tecpalinterview/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.tecpal.tecpalinterview.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Book API")
+                        .version("v0.0.1"));
+    }
+
+
+}
+
